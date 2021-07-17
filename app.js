bc8f50dd1bf282053d70a6159af704d7f4845255
DanielGG1024 <a22472510@gmail.com>
2021-07-17 21:08:08 +0800

    feat : modify views and show seed data in page

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Record = require('./modules/record')
 
-
+require('./config/mongoose')
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    Record.find()
+        .lean()
+        .then(records =>
+            res.render('index', { records }))
+        .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
