aa9c616b1ae279856afe2d0e50dc705eb1799a31
DanielGG1024 <a22472510@gmail.com>
2021-07-21 16:01:42 +0800

    typo mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const MONGODB = process.env.MONGODB || 'mongodb://localhost/expense-tracker'
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
