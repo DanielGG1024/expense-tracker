e37cf31d3af9c5e847fe983a9c31b7479d399026
DanielGG1024 <a22472510@gmail.com>
2021-07-23 14:41:47 +0800

    modify routes home

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,27 +5,50 @@ const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 //主頁
+// router.get('/', (req, res) => {
+//     const categoryQuery = req.query.category
+//     const filter = {}
+//     if (categoryQuery) filter.category = categoryQuery
+//     const categories = []
+     
+//     Category.find()
+//         .lean()
+//         .then(category => categories.push(...category))
+
+//     Record.find(filter)
+//         /********************/
+//         .populate('category')
+//         /********************/
+//         .lean()
+//         .then((records) => {
+//             let totalAmount = 0
+//             records.forEach(record => totalAmount += record.amount)
+//             res.render('index', { records, categories, totalAmount })
+//         })
+//         .catch(error => console.error(error))
+// })
+
 router.get('/', (req, res) => {
     const categoryQuery = req.query.category
     const filter = {}
     if (categoryQuery) filter.category = categoryQuery
-
     const categories = []
-    Category.find()
-        .lean()
-        .then(category => categories.push(...category))
 
-    Record.find(filter)
-        /********************/
-        .populate('category')
-        /********************/
+    Category
+        .find()
         .lean()
-        .then((records) => {
-            let totalAmount = 0
-            records.forEach(record => totalAmount += record.amount)
-            res.render('index', { records, categories, totalAmount })
+        .then((category) => {
+            Record
+                .find(filter)
+                .populate('category')
+                .lean()
+                .then((records) => {
+                    categories.push(...category)
+                    let totalAmount = 0
+                    records.forEach(record => totalAmount += record.amount)
+                    res.render('index', { records, categories, totalAmount })
+                })
+                .catch(error => console.error(error))
         })
-        .catch(error => console.error(error))
 })
-
 module.exports = router
